f1906a346da05d5018ccb7768761da4a5c573448
Yang861513665 <861513665@qq.com>
2018-04-20 11:03:36 +0800

    ‘加了一个testdemo’

--- java/src/cn/yxj/date/Test.java
+++ java/src/cn/yxj/date/Test.java
@@ -1,20 +1,28 @@
 package cn.yxj.date;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
 public static void main(String[] args) throws Exception {
-	Calendar calendar = Calendar.getInstance(new Locale("zh-cn"));
-	String  birtnday ="1996-10-12";
-	System.out.println(calendar.getTime());
-	Date  data=new SimpleDateFormat("yyyy-MM-dd").parse(birtnday);
-	calendar.setTime(data);
-	System.out.println(calendar.getFirstDayOfWeek());
-	System.out.println(calendar.get(Calendar.YEAR));
-	System.out.println(calendar.get(Calendar.MONTH)+1);
-	System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+	String sql ="select  * from user  order by uid,uuid";
+    Pattern p = Pattern.compile("order\\s*by[\\w|\\W|\\s|\\S]*");  
+    Matcher m = p.matcher(sql);  
+    StringBuffer sb = new StringBuffer(sql);  
+    if(m.find()){
+    	m.appendReplacement(sb, "");
+    }
+    System.out.println(sql+" ====> "+sb.substring(sql.length()));
+//	Calendar calendar = Calendar.getInstance(new Locale("zh-cn"));
+//	String  birtnday ="1996-10-12";
+//	System.out.println(calendar.getTime());
+//	Date  data=new SimpleDateFormat("yyyy-MM-dd").parse(birtnday);
+//	calendar.setTime(data);
+//	System.out.println(calendar.getFirstDayOfWeek());
+//	System.out.println(calendar.get(Calendar.YEAR));
+//	System.out.println(calendar.get(Calendar.MONTH)+1);
+//	System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
 }
 }
