1bfd88422138c638ff35f338dd1643db58719162
Yang861513665 <861513665@qq.com>
2018-05-21 13:55:40 +0800

    ‘executor  use demo’

--- java/src/cn/yxj/thread/TestDemo.java
+++ java/src/cn/yxj/thread/TestDemo.java
@@ -3,14 +3,16 @@ package cn.yxj.thread;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-
+/**
+ * 这里有1000个数字，交由一个线程池【一个有5个核心线程，最大允许创建10个线程】对其进行减操作。
+ * */
 public class TestDemo  implements Runnable{
 	int  j =1000;
 	Object lock= new Object();
     public static void main(String[] args) {
     	ThreadPoolExecutor  executor =  new ThreadPoolExecutor(5, 10, 1, TimeUnit.MINUTES , new ArrayBlockingQueue<Runnable>(100));
     	TestDemo task = new TestDemo();
-    	for (int i = 0; i < 100; i++) {
+    	for (int i = 0; i < 10; i++) {
 			executor.submit(task);
 		}
     	executor.shutdown();
@@ -19,7 +21,7 @@ public class TestDemo  implements Runnable{
 	@Override
 	public void run() {
 		synchronized (lock) {
-			for (int i = 0; i < 10; i++) {
+			for (int i = 0; i < 100; i++) {
 				j--;		
 				System.out.println(Thread.currentThread().getName()+"-->"+j);
 			}
